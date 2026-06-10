6b5c4e09cdb1401955a7d405ca4836a0d808966c
Yogesh Pal <159111601+theyogeshpal@users.noreply.github.com>
2026-06-10 18:24:45 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const target = env.VITE_API_BASE || 'https://smsspi.thedigicoders.com';
+  const target = env.VITE_API_BASE || 'https://rcsbackend-txzv.onrender.com';
 
   return {
     plugins: [react()],
